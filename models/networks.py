869f3bca3deb8901f434a4f4b72073ea2ac00198
mohammd-young <seyed.mr.tinker@gmail.com>
2025-07-29 16:45:54 +0330

    networks.py

--- models/networks.py
+++ models/networks.py
@@ -362,7 +362,7 @@ class UnetSkipConnectionBlock(nn.Module):
                                         kernel_size=4, stride=2,
                                         padding=1)
             down = [downconv]
-            up = [uprelu, upconv, nn.Tanh()]
+            up = [uprelu, upconv, nn.Sigmoid()]
             model = down + [submodule] + up
         elif innermost:
             upconv = nn.ConvTranspose2d(inner_nc, outer_nc,
