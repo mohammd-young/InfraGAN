dfc1625cf0f62411d35c3e45f3d5ca58aded8e68
mohammd-young <seyed.mr.tinker@gmail.com>
2025-08-17 12:03:20 -0700

    ssim.py

--- ssim.py
+++ ssim.py
@@ -16,7 +16,7 @@ def create_window(window_size, channel=1):
     return window
 
 
-def ssim(img1, img2, window_size=11, window=None, size_average=True, full=False, val_range=1):
+def ssim(img1, img2, window_size=11, window=None, size_average=True, full=False, val_range=2):
     # Value range can be different from 255. Other common ranges are 1 (sigmoid) and 2 (tanh).
     if val_range is None:
         if torch.max(img1) > 128:
